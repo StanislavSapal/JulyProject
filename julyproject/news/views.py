c0085dbc1d509b52623f26f5dbef9ff90324fba9
StanislavSapal <sapalstanislav22@gmail.com>
2022-07-26 18:07:24 +0300

    first commit

---
+++ julyproject/news/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    #print(request)
+    return HttpResponse('<h2>Hello world</h2>')
+
+def test(request):
+    return HttpResponse('<h1>Тестова сторінка</h1>')
+
