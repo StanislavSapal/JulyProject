5d2225f76c31931635a409abf25935c13c42a36c
StanislavSapal <sapalstanislav22@gmail.com>
2022-08-25 20:19:23 +0300

    after the 26th video

--- news/models.py
+++ news/models.py
@@ -9,12 +9,11 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Оновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубліковано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категорія')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категорія')
+
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={'news_id': self.pk})
-
-
 
     def __str__(self):
         return self.title
@@ -23,6 +22,7 @@ class News(models.Model):
         verbose_name = 'Новина'
         verbose_name_plural = 'Новини'
         ordering = ['-created_at']
+
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Назва категорії')
--- news/urls.py
+++ news/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('', index, name='home'),
     path('test/', test),
     path('rename_latest_news/', rename_to_govno),
-    path('delete_govno/', delete_govno),
+    path('delete_govno/', delete_govno, name='delete_govno'),
     path('category/<int:category_id>/', get_category, name='category'),
-    path('news/<int:news_id>/', view_news, name='view_news')
+    path('news/<int:news_id>/', view_news, name='view_news'),
+    path('news/add-news/', add_news, name='add_news')
 
 ]
