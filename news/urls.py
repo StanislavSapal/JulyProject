dd7de4166a06f1149e491b1459eed0bbb48bf154
StanislavSapal <sapalstanislav22@gmail.com>
2022-09-06 14:17:38 +0300

    after 30

--- news/urls.py
+++ news/urls.py
@@ -3,15 +3,16 @@ from .views import *
 
 
 urlpatterns = [
-    # path('', index, name='home'),
     path('', HomeNews.as_view(), name='home'),
-    path('test/', test),
-    path('rename_latest_news/', rename_to_govno),
-    path('delete_govno/', delete_govno, name='delete_govno'),
-    # path('category/<int:category_id>/', get_category, name='category'),
     path('category/<int:category_id>/', NewsByCategory.as_view(extra_context={'title': 'Обрана категорія'}),
          name='category'),
-    path('news/<int:news_id>/', view_news, name='view_news'),
-    path('news/add-news/', add_news, name='add_news')
-
+    path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
+    path('news/add-news/', CreateNews.as_view(), name='add_news')
+    # path('', index, name='home'),
+    # path('test/', test),
+    # path('rename_latest_news/', rename_to_govno),
+    # path('delete_govno/', delete_govno, name='delete_govno'),
+    # path('news/<int:news_id>/', view_news, name='view_news'),
+    # path('category/<int:category_id>/', get_category, name='category'),
+    # path('news/add-news/', add_news, name='add_news')
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from news.models import News, Category
 from .forms import NewsForm
@@ -10,15 +11,15 @@ class HomeNews(ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
-    #extra_context = {'title': 'Головна'}
+    # extra_context = {'title': 'Головна'}
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Головна сторінка'
         return context
+
     def get_queryset(self):
         return News.objects.filter(is_published=True)
-
 
 
 class NewsByCategory(ListView):
@@ -35,13 +36,18 @@ class NewsByCategory(ListView):
     def get_queryset(self):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
 
-#def index(request):
-#    news = News.objects.all()
-#    context = {
-#        'news': news,
-#        'title': 'Список новин',
-#        }
-#    return render(request, 'news/index.html', context)
+
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
+    # pk_url_kwarg = 'news_id'
+    # template_name = 'news/news_detail.html'
+
+
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home')
 
 
 def get_category(request, category_id):
@@ -49,61 +55,71 @@ def get_category(request, category_id):
     category = Category.objects.get(pk=category_id)
     return render(request, 'news/category.html', {'news': news, 'category': category})
 
-def test(request):
-    latest_news = News.objects.filter(title='Новина 1').first()
+# def index(request):
+#    news = News.objects.all()
+#    context = {
+#        'news': news,
+#        'title': 'Список новин',
+#        }
+#    return render(request, 'news/index.html', context)
 
-    if latest_news:
-        result_title = latest_news.title
-    else:
-        result_title = 'хуй тобі собачій'
-    result = f'<h1>{result_title}</h1>'
+#def view_news(request, news_id):
+    #news_item = News.objects.get(pk=news_id)
+#    news_item = get_object_or_404(News, pk=news_id)
+#    return render(request, 'news/view_news.html', {'news_item': news_item})
 
-    return HttpResponse(result)
+
+# def add_news(request):
+#    if request.method == 'POST':
+#        form = NewsForm(request.POST)
+#        if form.is_valid():
+#            # print(form.cleaned_data)
+#            # news = News.objects.create(**form.cleaned_data)
+#            news = form.save()
+#            return redirect(news)
+#    else:
+#        form = NewsForm()
+#    return render(request, 'news/add_news.html', {'form': form})
+
+
+#def test(request):
+#    latest_news = News.objects.filter(title='Новина 1').first()
+
+#    if latest_news:
+#        result_title = latest_news.title
+#    else:
+#        result_title = 'хуй тобі собачій'
+#    result = f'<h1>{result_title}</h1>'
+
+#    return HttpResponse(result)
 
 # Cтворити вьюху яка бере саму стару новину. Якшо її тітл не Говно то перейменовує в Говно і веpтає тітл
 # Якщо її тітл Говно - пише шо так і було
 
-def rename_to_govno(request):
-    latest_news = News.objects.all().order_by('-created_at').first()
 
-    if latest_news.title == 'Говно':
-        result = f'<h2>Заголовок і так був {latest_news.title}</h2>'
-    else:
-        latest_news.title = 'Говно'
-        latest_news.save()
-        result = f'<h2>Вітаємо, заголовок змінено на {latest_news.title}</h2>'
+#def rename_to_govno(request):
+#    latest_news = News.objects.all().order_by('-created_at').first()
 
-    return HttpResponse(result)
+#    if latest_news.title == 'Говно':
+#        result = f'<h2>Заголовок і так був {latest_news.title}</h2>'
+#    else:
+#        latest_news.title = 'Говно'
+#        latest_news.save()
+#        result = f'<h2>Вітаємо, заголовок змінено на {latest_news.title}</h2>'
+
+#    return HttpResponse(result)
+
 # Створити вьюху delete_govno яка якшо новинa з тітлом Говно є, то видаляє і пише шо удалила, якшо нема, пише "такої
 # новини немає"
 
-def delete_govno(request):
-    govno_news = News.objects.filter(title='Говно').first()
+#def delete_govno(request):
+#    govno_news = News.objects.filter(title='Говно').first()
+#
+#    if not govno_news:
+#        result = f'<h2>Новини з таким заголовком нема, ти шо на приколі? Хто так називає новини?</h2>'
+#    else:
+#        govno_news_title = govno_news.title
+#        govno_news.delete()
+#        result = f'<h2>Новину з заголовком {govno_news_title} видалено</h2>'
 
-    if not govno_news:
-        result = f'<h2>Новини з таким заголовком нема, ти шо на приколі? Хто так називає новини?</h2>'
-    else:
-        govno_news_title = govno_news.title
-        govno_news.delete()
-        result = f'<h2>Новину з заголовком {govno_news_title} видалено</h2>'
-
-    return HttpResponse(result)
-
-def view_news(request, news_id):
-    #news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {'news_item': news_item})
-
-
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            # print(form.cleaned_data)
-            # news = News.objects.create(**form.cleaned_data)
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
-
+#    return HttpResponse(result)
