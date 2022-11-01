e805d45bf8aa892040fb48a91d64028f68d2a11c
StanislavSapal <sapalstanislav22@gmail.com>
2022-11-01 20:01:43 +0200

    Revert "created NewTest"
    This reverts commit 654da59393e1f62cd13ec74c33166016cca857a3.

--- news/tests.py
+++ news/tests.py
@@ -7,11 +7,3 @@ class Test(TestCase):
     # code for this class
     # code for this class
     # code for this class
-
-
-class NewTest(TestCase):
-    pass
-    # code for this class
-    # code for this class
-    # code for this class
-
