2abadcfcdde49623a156f3aea362568ca658b68a
StanislavSapal <sapalstanislav22@gmail.com>
2022-11-01 20:06:59 +0200

    created NewTest again

--- news/tests.py
+++ news/tests.py
@@ -7,3 +7,10 @@ class Test(TestCase):
     # code for this class
     # code for this class
     # code for this class
+
+
+class NewTest(TestCase):
+    pass
+    # code for this class
+    # code for this class
+    # code for this class
